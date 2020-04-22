b5a73c143f8c14249c344c3b4262fb304a2a323e
s1898201 <c.han-8@sms.ed.ac.uk>
2020-04-22 11:58:58 +0100

    Now json loads properly to csv.

--- daraPreProcess/json2csv.R
+++ daraPreProcess/json2csv.R
@@ -9,5 +9,11 @@ json_file = "https://api.covid19uk.live/historyfigures"
 readData =  fromJSON(paste(readLines(json_file), collapse="", simplify=TRUE))
 
 hisData = readData[["data"]]
+for (item in 1:length(hisData)) {
+  hisData[[item]] = unlist(as.character(hisData[[item]]), use.names = T)
+}
 
-write.csv2(hisData, file = "historyfigures.csv")
+dfData = as.data.frame( Reduce(rbind, hisData))
+colnames(dfData) = names(readData[["data"]][[1]])
+rownames(dfData) = NULL
+write.csv(dfData, file = "historyfigures.csv", row.names = F, quote = F)
---
+++ visualisation/log_vis.R
@@ -0,0 +1,5 @@
+setwd("~/Documents/GitHub/Covid19predict/visualisation")
+library(ggplot2)
+
+
+dfUK = read.csv("../daraPreProcess/historyfigures.csv", header = T)
