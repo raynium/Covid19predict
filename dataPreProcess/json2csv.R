33ca5df63ae77437b5d9407cb73fb3c6100cf30e
Chaolin Han <chaosrinkhan@gmail.com>
2020-05-07 13:52:08 +0100

    Daily updates

--- dataPreProcess/json2csv.R
+++ dataPreProcess/json2csv.R
@@ -4,7 +4,6 @@ library(rjson)
 library (plyr)
 library(data.table)
 library(reshape2) # library for reshaping data (tall-narrow <-> short-wide)
-library(deSolve) # library for solving differential equations
 json_file = "https://api.covid19uk.live/historyfigures"
 readData =  fromJSON(paste(readLines(json_file), collapse="", simplify=TRUE))
 
